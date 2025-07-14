81883cd84cd2335319fd40ed70c18734fe0df216
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-14 16:39:49 +0545

    Added manifest file

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':'Custom Theme',
+    'description':'A custom theme build on odoo 18',
+    'category':'Theme',
+    'version':'1.0',
+    'author':'Saksham Khanal',
+    'license':'LGPL-3',
+    'depends':['website']
+
+}
