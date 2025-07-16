066d8957217cdbbfbecebc4d41d3dee41c278c4c
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-16 18:41:35 +0545

    Color palletes working in the theme

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,21 @@
 {
-    'name':'Custom Theme',
+    'name':'Website Airproof',
     'description':'A custom theme build on odoo 18',
     'category':'Theme',
     'version':'1.0',
     'author':'Saksham Khanal',
     'license':'LGPL-3',
-    'depends':['website'],
     'installable':True,
-
+    'depends':['website_sale','website_sale_wishlist'],
+    'data':[
+    'data/presets.xml',
+    ],
     'assets':{
     'web._assets_primary_variables':[
         'website_airproof/static/src/scss/primary_variables.scss',
+        ],
+    'web._assets_frontend_helpers': [
+      ('prepend', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
         ],
      },
 }
